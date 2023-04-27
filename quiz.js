29b6d0dc6cb568f812333762c49a7d8b8c850549
hopehsiao04 <hlhsiao@usc.edu>
2023-04-27 15:50:53 -0700

    learn and quiz

---
+++ quiz.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.feedback').hide();
+    $('input[type=radio]').change(function() {
+        var questionContainer = $(this).closest('div');
+        var selected = questionContainer.find('input[type=radio]:checked').val();
+        if(selected == 'b') {
+            questionContainer.find('.feedback[data-answer="b"]').fadeIn();
+            questionContainer.find('.feedback[data-answer="a,c"]').fadeOut();
+        } else {
+            questionContainer.find('.feedback[data-answer="b"]').fadeOut();
+            questionContainer.find('.feedback[data-answer="a,c"]').fadeIn();
+        }
+        var questionContainer = $(this).closest('div');
+        var selected = questionContainer.find('input[type=radio]:checked').val();
+        if(selected == 'c') {
+            questionContainer.find('.feedback[data-answer="c"]').fadeIn();
+            questionContainer.find('.feedback[data-answer="a,b"]').fadeOut();
+        } else {
+            questionContainer.find('.feedback[data-answer="c"]').fadeOut();
+            questionContainer.find('.feedback[data-answer="a,b"]').fadeIn();
+        }
+        var questionContainer = $(this).closest('div');
+        var selected = questionContainer.find('input[type=radio]:checked').val();
+        if(selected == 'a') {
+            questionContainer.find('.feedback[data-answer="a"]').fadeIn();
+            questionContainer.find('.feedback[data-answer="b,c"]').fadeOut();
+        } else {
+            questionContainer.find('.feedback[data-answer="a"]').fadeOut();
+            questionContainer.find('.feedback[data-answer="b,c"]').fadeIn();
+        }
+    
+    });
+});
