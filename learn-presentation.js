47902673aa2ea273f80b7fbba46b958680686455
wangshei <wangshei@usc.edu>
2023-05-01 14:57:39 -0700

    working learn animation

--- learn-presentation.js
+++ learn-presentation.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
-    
-    $("#circle").delay(11000).animate({"top":"1000px"},500);
+
+     $(".circle").hide();
+     $("#arrowteam").hide();
     $("#tutorialbutton").click(function() {
       
-        $("#audio-11").trigger("play");
+      $("#audio-11").trigger("play");
+        
       
       setTimeout(function() {
+        $(".circle").fadeIn().animate({"top":"1000px"},5000)
         $("#audio-12").trigger("play");
-      }, 11000);
+      }, 11000); 
+
+      setTimeout(function() {
+        $(".circle").fadeIn().animate({"top":"1700px"},1000)
+      }, 25000); 
+
+      setTimeout(function() {
+        $("#audio-13").trigger("play");
+        $("#arrowteam").fadeIn();
+      }, 27000); 
+
     })
+
   });
