6413a57a081298e57f003c3a70f6c767d05b4d02
Hank N <henrywarren04@yahoo.com>
2023-05-01 16:50:36 -0700

    bouncy arrow class
    use class = "arrow horizontal" or "arrow vertical"

--- aboutpresentation.js
+++ aboutpresentation.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
 
     $("#audio-4")[0].play();
+
+    setTimeout(function() {
+        // place right of fox
+        $("#arrow6").css({
+            "top": $("#fox").offset().top + ($("#fox").height() / 2) - ($("#arrow1").height() / 2),
+            "left": $("#fox").offset().left + $("#fox").width() - 1500,
+            // display block
+            "display": "block"
+        });
+        // place right of charactertitle
+        $("#arrow7").css({
+            "top": $("#characters-title").offset().top + ($("#characters-title").height() / 2) - ($("#arrow1").height() / 2),
+            "left": $("#characters-title").offset().left + $("#characters-title").width() + 10,
+            "display": "block"
+        });
+    }, 1000);
+    
     setTimeout(function() {
         $("#audio-5")[0].play();
         $('html, body').animate({
