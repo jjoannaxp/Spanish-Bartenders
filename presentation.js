f8f4c02e19aa08b52c0f9be4efe994501ffcd2ad
joannasp12 <jpoon871@student.fuhsd.org>
2023-04-30 22:57:34 -0700

    presentation start

--- presentation.js
+++ presentation.js
@@ -1,6 +1,12 @@
+$(document).ready(function() {
+    $("#tutorialbutton").click(function() {
+       $("#audio-1").trigger("play");
+      
+      $("#arrow1").animate({top: "60px"}, 500)
 
-$(document).on("click",function(){
-    $("#arrow1").animate({
-        "top": "2vh",
-    }, 2000);
-});
+      // $("#audio-2").trigger("play");
+      $("#circle").animate({
+        "display": "block"
+      })
+    });
+  });
