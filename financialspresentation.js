7704880eb4d896c77125d956d248d8ab2afca4d9
hopehsiao04 <hlhsiao@usc.edu>
2023-05-02 00:05:39 -0700

    hi

--- financialspresentation.js
+++ financialspresentation.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
-  $(".circle").hide();
 
+  $(".circle").hide();
+  $("#tutorialbutton").click(function () {
   $("#business2").trigger("play");
 
   setTimeout(function () {
     $(".circle").fadeIn().animate({ "margin-left": "730px" }, 300);
   }, 6200);
+  setTimeout(function() {
+    window.location.href = "Presentation.html";
+  }, 10000);
+
 });
+
 const tutorialButton = document.getElementById("tutorialbutton");
 
 tutorialButton.addEventListener("click", function() {
 
 $("#tutorialbutton").hide();
 })
+});
